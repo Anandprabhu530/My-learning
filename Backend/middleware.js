3ede532a69bbbb90074d75375d2ea08fa74ed5fb
Anand Prabhu <anandprabhu530@gmail.com>
2024-05-24 22:19:10 +0530

    Global Catches

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -3,24 +3,31 @@ const z = require("zod");
 const app = express();
 
 app.use(express.json());
-// const login = z.object({ email: z.string, password: z.string });
-
 const schema = z.object({ email: z.string(), password: z.string() });
-const checker_middleware = (req, res, next) => {
-  const input_data = req.body;
-  const check = schema.safeParse(input_data);
-  if (!check.success) {
-    res.status(412).send("Invalid Input format");
-  }
+// const checker_middleware = (req, res, next) => {
+//   const input_data = req.body;
+//   const check = schema.safeParse(input_data);
+//   if (!check.success) {
+//     res.status(412).send("Invalid Input format");
+//   }
 
-  console.log("Hello World from middleware");
-  next();
-};
+//   console.log("Hello World from middleware");
+//   next();
+// };
 
-app.get("/", checker_middleware, (req, res) => {
+app.get("/", (req, res) => {
+  const data = req.body;
+  const ans = body.password[0].length;
   res.status(200).json({ msg: "All Working" });
 });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+});
+
+//global-catch
+app.use(function error_checker(err, req, res, next) {
+  res.json({
+    message: "An Error Occured with the server",
+  });
 });
