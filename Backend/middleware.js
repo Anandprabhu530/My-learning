e421b2a2efa447d5bc2be7f06bc9b008b1be28dc
Anand Prabhu <anandprabhu530@gmail.com>
2024-05-24 22:11:03 +0530

    Input validation with zod

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
+const z = require("zod");
 const app = express();
 
+app.use(express.json());
+// const login = z.object({ email: z.string, password: z.string });
+
+const schema = z.object({ email: z.string(), password: z.string() });
 const checker_middleware = (req, res, next) => {
-  //check auth and other things
+  const input_data = req.body;
+  const check = schema.safeParse(input_data);
+  if (!check.success) {
+    res.status(412).send("Invalid Input format");
+  }
+
   console.log("Hello World from middleware");
   next();
 };
