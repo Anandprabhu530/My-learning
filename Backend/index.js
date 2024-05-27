ae562e4557b0393d0649d9aadc22ec7352f91e5a
Anand Prabhu <97842562+Anandprabhu530@users.noreply.github.com>
2024-05-27 09:19:40 +0000

    Added JWT after Signup

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,8 @@ require("dotenv").config()
 app.use(express.json())
 mongoose.connect(process.env.MONGO_DB_URL);
 
+const User = mongoose.model('User',{email:String,password:String})
+
 app.get("/sign-in", (req, res) => {
   const token = req.headers.authorization
   const decrypted_token = jwt.verify(token,process.env.JWT_SECRET_TOKEN);
@@ -14,8 +16,18 @@ app.get("/sign-in", (req, res) => {
   res.status(200).json({ msg: "All Ok" });
 });
 
-app.get("sign-up",(req,res)=>{
-  //sign up the user
+app.get("/sign-up",async(req,res)=>{
+  //check if user exists
+  const Userexists = await mongoose.findOne({email:String})
+  if(Userexists){
+    res.status(400).json({Server:"User already exists. Please Login!"})
+  }
+  //save user to database - not adviced to store password in database(can hash the password and store it..)
+  const user = new User({email:req.body.email,password:req.body.password});
+  user.save();
+
+  const token = jwt.sign(req.body.email + "" + req.body.password,process.env.JWT_SECRET_TOKEN)
+  res.status(200).json(token)
 })
 
 app.listen(3000, () => {
